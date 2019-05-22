2c776d0a6d51cde481548f0b650c2614966e7e6e
sonali-Packt <sonalis@packtpub.com>
2019-05-22 16:53:54 +0530

    Code Uploaded

---
+++ Section05.sql
@@ -0,0 +1,11 @@
+Adventure/* Section 5. Sample Databases
+					SQL Source code*/
+
+--Drop database if existed
+DROP DATABASE AdventureWorks
+
+--Restore database AdventureWorks from backup file
+RESTORE DATABASE AdventureWorks
+FROM DISK = '/var/opt/mssql/backup/AdventureWorks2014.bak'
+WITH MOVE 'AdventureWorks2014_Data' TO '/var/opt/mssql/data/AdventureWorks2014_Data.mdf',
+MOVE 'AdventureWorks2014_Log' TO '/var/opt/mssql/data/AdventureWorks2014_Log.ldf'
